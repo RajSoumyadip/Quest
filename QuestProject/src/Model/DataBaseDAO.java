8cf1b856cd84ee6161650ca6a081213165b77ae2
Soumyadip <Soumyadipghosh220114@gmail.com>
2018-11-22 00:25:03 +0530

    DB Initialisation

--- QuestProject/src/Model/DataBaseDAO.java
+++ QuestProject/src/Model/DataBaseDAO.java
@@ -8,20 +8,18 @@ import java.sql.PreparedStatement;
 
 	public class DataBaseDAO {
 
-		public boolean insertRecord(String c,String c1,String top,String rel)
+		public boolean insertRecord(String c)
 		{
 			boolean result=true;
 			try{
 				Class.forName("com.mysql.jdbc.Driver");
-				String url="jdbc:mysql://localhost:3306/SoftwareEngineering";
+				String url="jdbc:mysql://localhost:3306/QuestDB";
 				String user="root";
 				String pass="root";
 				Connection con=(Connection) DriverManager.getConnection(url,user,pass);
 				PreparedStatement ps=con.prepareStatement("Insert into Data values(?,?,?,?)");
 				ps.setString(1, c);
-				ps.setString(2, c1);
-				ps.setString(3, top);
-				ps.setString(4, rel);
+				
 				ps.executeUpdate();
 			}catch(Exception ex)
 			{
